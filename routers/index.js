c68a64970cbbc1e1a0df76b700a20698f87d3052
Akash Padampalle <akashpadampalle780@gmail.com>
2023-04-18 16:41:47 +0530

    add login and signup form

--- routers/index.js
+++ routers/index.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const homeController = require('../controllers/home_controllers');
 
 router.get('/', homeController.homepage);
+router.get('/login', homeController.login);
+router.get('/signup', homeController.signup);
 
 module.exports = router;
