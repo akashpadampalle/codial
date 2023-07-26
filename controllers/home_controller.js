c14c743a8a331c0e6031d43d52b984254c09602a
Akash Padampalle <akashpadampalle780@gmail.com>
2023-07-26 09:00:51 +0530

    nested population of post comments

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,7 +2,14 @@ const Post = require("../models/post");
 
 module.exports.home = async function (req, res) {
 
-    const posts = await Post.find({}).populate('user');
+    const posts = await Post.find({})
+    .populate('user')
+    .populate({
+        path: 'comments',
+        populate: {
+            path: 'user'
+        }
+    });
 
     return res.render('home', {
         title: "Codial | Home",
