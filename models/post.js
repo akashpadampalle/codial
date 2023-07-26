cc524e0248b3e3e08efa07a72c4351441a909e51
Akash Padampalle <akashpadampalle780@gmail.com>
2023-07-26 08:19:01 +0530

    create comment schema

--- models/post.js
+++ models/post.js
@@ -9,7 +9,13 @@ const postSchema = new mongoose.Schema({
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true
-    }
+    },
+    comments: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: 'Comment'
+        }
+    ]
 }, { timestamps: true });
 
 const Post = mongoose.model('Post', postSchema);
