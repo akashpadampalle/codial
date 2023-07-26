83000b4d4c73652ff84480907b768e2cfbb1f02e
Akash Padampalle <akashpadampalle780@gmail.com>
2023-07-26 12:43:51 +0530

    add delete post

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -1,11 +1,27 @@
 const Post = require('../models/post');
+const Comment = require('../models/comment');
 
 module.exports.create = async function (req, res) {
     try {
         const post = await Post.create({ content: req.body.content, user: req.user._id });
-        res.redirect('back');
     } catch (err) {
         console.log(`error in create post ${err}`);
+    }finally{
+        res.redirect('back');
+    }
+}
+
+
+module.exports.destroy = async function(req, res){
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post && post.user == req.user.id){
+            await Post.findByIdAndDelete(post.id);
+            await Comment.deleteMany({post: req.params.id})
+        }
+    }catch(err){
+        console.log(`error in destroying post ${err}`);
+    }finally{
         res.redirect('back');
     }
 }
